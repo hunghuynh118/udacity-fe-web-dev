f72eb34d0cb2c36b53cc7a6e20f1f75b33bffc48
Hung Huynh <hunghuynh.11899@gmail.com>
2023-09-25 11:30:07 +0700

    add env

--- Project 4/src/server/index.js
+++ Project 4/src/server/index.js
@@ -1,4 +1,11 @@
-var path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const textapi = new meaningcloud({
+    application_key: process.env.API_KEY,
+});
+
+const path = require("path");
 const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
 
