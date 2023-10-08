62cb5df60807892448425eda74e136991377ce82
Hung Huynh <hunghuynh.11899@gmail.com>
2023-10-08 16:31:42 +0700

    archive

--- capstone-project/README.md
+++
@@ -1,5 +0,0 @@
-1. Making the boilerplate for the project.
-2. Setting up webpack and installing all necessary packages.
-
-https://pixabay.com/api/docs/
-http://www.geonames.org/export/web-services.html
--- capstone-project/src/client/scripts/calculateRemainingDays.js
+++
@@ -1,8 +0,0 @@
-const calculateRemainingDays = date => {
-    const daysDiff = new Date(date) - new Date();
-    let remainingDays = new Date(daysDiff) / (24 * 3600 * 1000);
-    remainingDays = Number(Math.ceil(remainingDays)) + 1;
-    return remainingDays;
-};
-
-export { calculateRemainingDays };
