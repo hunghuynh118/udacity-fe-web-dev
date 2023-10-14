3bf8f47843acf67fb2dc60c0d090dcb1ee169ca4
Hung Huynh <hunghuynh.11899@gmail.com>
2023-10-14 09:47:53 +0700

    add workbox plugin

--- Capstone Project/webpack.common.js
+++ Capstone Project/webpack.common.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
     entry: ["./src/client/index.js"],
@@ -36,5 +37,6 @@ module.exports = {
                 { from: "src/client/media/images", to: "images" },
             ],
         }),
+        new WorkboxPlugin.GenerateSW(),
     ],
 };
